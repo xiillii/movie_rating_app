cab4f9855b603d62898e1d42b5f023627468a884
xiillii <xiillii@outlook.com>
2018-08-04 12:20:27 -0500

    Form added to insert a movie

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,28 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import HelloWorld from '@/components/HelloWorld';
+import Home from '@/components/Home';
+import Contact from '@/components/Contact';
+import AddMovie from '@/components/AddMovie';
 
 Vue.use(Router);
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld,
+      name: 'Home',
+      component: Home,
+    },
+    {
+      path: '/contact',
+      name: 'Contact',
+      component: Contact,
+    },
+    {
+      path: '/movies/add',
+      name: 'AddMovie',
+      component: AddMovie,
     },
   ],
 });
